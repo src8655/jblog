483c0a7068e0f807a7603929339c2d21e3f96b7f
src8655 <src8655@naver.com>
2019-06-03 14:47:05 +0900

    11

---
+++ jblog3/src/main/java/com/cafe24/jblog/vo/PostVo.java
@@ -0,0 +1,53 @@
+package com.cafe24.jblog.vo;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+public class PostVo {
+	private Long no = -1L;
+	@NotEmpty
+	private String title;
+	@NotEmpty
+	private String content;
+	private String regDate;
+	private Long categoryNo;
+	
+	private String categoryName;
+	public Long getNo() {
+		return no;
+	}
+	public void setNo(Long no) {
+		this.no = no;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(String regDate) {
+		this.regDate = regDate;
+	}
+	public Long getCategoryNo() {
+		return categoryNo;
+	}
+	public void setCategoryNo(Long categoryNo) {
+		this.categoryNo = categoryNo;
+	}
+	public String getCategoryName() {
+		return categoryName;
+	}
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+	
+	
+}
---
+++ jblog3/src/main/java/com/cafe24/mysite/config/WebConfig.java
@@ -0,0 +1,36 @@
+package com.cafe24.mysite.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import com.cafe24.config.web.FileuploadConfig;
+import com.cafe24.config.web.MVCConfig;
+import com.cafe24.config.web.SecurityConfig;
+
+@Configuration
+@EnableWebMvc
+@EnableAspectJAutoProxy
+@ComponentScan({"com.cafe24.mysite.controller","com.cafe24.mysite.controller.api"})
+@Import({MVCConfig.class, SecurityConfig.class, FileuploadConfig.class})
+public class WebConfig{
+	
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		resolver.setExposeContextBeansAsAttributes(true);
+		return resolver;
+	}
+
+	
+}
