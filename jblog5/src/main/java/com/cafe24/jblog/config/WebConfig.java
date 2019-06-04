c98c7c89e3f974bce2c80d1542020f83c2d482a7
src8655 <src8655@naver.com>
2019-06-04 15:50:48 +0900

    11

--- jblog5/src/main/java/com/cafe24/jblog/config/WebConfig.java
+++ jblog5/src/main/java/com/cafe24/jblog/config/WebConfig.java
@@ -5,26 +5,17 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
-import com.cafe24.config.web.FileuploadConfig;
 import com.cafe24.jblog.interceptor.BlogInterceptor;
 import com.cafe24.security.AuthInterceptor;
 import com.cafe24.security.AuthLoginInterceptor;
